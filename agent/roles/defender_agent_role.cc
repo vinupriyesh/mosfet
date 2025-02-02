7f287410bff6d4dfc1cda6893f4d43ea7ed22a4b
Vinu <vinpriyesh@gmail.com>
2025-02-02 14:29:05 +0530

    Adding the defender role logic

--- agent/roles/defender_agent_role.cc
+++ agent/roles/defender_agent_role.cc
@@ -71,5 +71,41 @@ bool DefenderAgentRole::chooseAttackingTile(Communicator &communicator) {
 
 
 void DefenderAgentRole::surveyJobBoard(JobBoard& jobBoard) {
-    //No op
+
+    GameEnvConfig& gameEnvConfig = GameEnvConfig::getInstance();
+    if (shuttle.energy <= gameEnvConfig.unitSapCost + gameEnvConfig.unitMoveCost) {
+        //Shuttle doesn't have enough energy to attack and move
+        return;
+    }
+
+    
+
+    for (Job* job : jobBoard.getJobs()) {
+        if (job->type == JobType::DEFENDER) {
+            DefenderJob* defenderJob = static_cast<DefenderJob*>(job);
+
+            if (std::abs(defenderJob->targetX - shuttle.getX()) <= gameEnvConfig.unitSapRange && std::abs(defenderJob->targetY - shuttle.getY()) <= gameEnvConfig.unitSapRange) {
+                
+                GameTile& targetTile = gameMap.getTile(defenderJob->targetX, defenderJob->targetY);
+                
+                //குறி வெச்சா இரை விழனும் 
+                bool canSap = false;
+                for (auto& opponent:targetTile.opponentShuttles) {
+                    if (opponent->energy <= gameEnvConfig.unitSapCost) {
+                        canSap = true;
+                        break;    
+                    }
+                }
+                if (!canSap) {
+                    continue;
+                }
+
+                std::tuple<int, int> relativePosition = getRelativePosition(targetTile);
+                std::vector<int> bestPlan = {5, std::get<0>(relativePosition), std::get<1>(relativePosition)};
+                log("Attacking tile - " + std::to_string(defenderJob->targetX) + ", " + std::to_string(defenderJob->targetY));
+                JobApplication* jobApplication = &jobBoard.applyForJob(job, &shuttle, std::move(bestPlan));
+                jobApplication->setPriority(shuttle.energy); 
+            }            
+        }
+    }
 }
